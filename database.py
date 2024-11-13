36080297f731af2131421d664bed281bb67f8576
santtiag <lucass.hernandez@pm.me>
2024-11-13 14:08:02 -0500

    create a global base file

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models.user import User
+from base import Base
 
 # DATABASE_URL = "mysql+pymysql://root:mysql_es_p3ro_Muy_buena-@localhost:3306/electrical_energy_db"
 DATABASE_URL = "postgresql://numerical_user:sqCXrboVe0gT5lDAXacdFSPqocF0gK6O@dpg-csqdg1ilqhvc738emojg-a/numerical"
@@ -10,7 +11,6 @@ DATABASE_URL = "postgresql://numerical_user:sqCXrboVe0gT5lDAXacdFSPqocF0gK6O@dpg
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
 Base.metadata.create_all(engine)
 
