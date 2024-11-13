f45168035e3924f26a0452cedf74c69a959141f1
santtiag <lucass.hernandez@pm.me>
2024-11-13 13:55:42 -0500

    instance the db in database file

--- database.py
+++ database.py
@@ -2,15 +2,17 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from models.user import User
 
 # DATABASE_URL = "mysql+pymysql://root:mysql_es_p3ro_Muy_buena-@localhost:3306/electrical_energy_db"
 DATABASE_URL = "postgresql://numerical_user:sqCXrboVe0gT5lDAXacdFSPqocF0gK6O@dpg-csqdg1ilqhvc738emojg-a/numerical"
 # DATABASE_URL = os.getenv("DATABASE_URL")
 
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+Base.metadata.create_all(engine)
 
 def get_db():
     db = SessionLocal()
--- routers/auth.py
+++ routers/auth.py
@@ -2,23 +2,23 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 import utils
 from database import get_db
-from models import user as user_models
-from schemas import user as user_schemas
+from models.user import User
+from schemas.user import UserCreate, UserLogin
 
 auth_router = APIRouter()
 
 @auth_router.post("/register/")
-def register(user: user_schemas.UserCreate, db: Session = Depends(get_db)):
+def register(user: UserCreate, db: Session = Depends(get_db)):
     hashed_password = utils.hash_password(user.password)
-    db_user = user_models.User(email=user.email, hashed_password=hashed_password)
+    db_user = User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return {"message": "User created successfully"}
 
 @auth_router.post("/login/")
-def login(user: user_schemas.UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(user_models.User).filter(user_models.User.email == user.email).first()
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
     if not db_user or not utils.verify_password(user.password, db_user.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
